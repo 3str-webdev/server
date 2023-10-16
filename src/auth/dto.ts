27f3714b3614a930bdf21f94657e8182e9f3e2b3
3str-webdev <3str.webdev@gmail.com>
2023-10-16 17:36:05 +0300

    feat: add validation

--- src/auth/dto.ts
+++ src/auth/dto.ts
@@ -1,18 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignUpBodyDto {
   @ApiProperty({ example: 'test@gmail.com' })
+  @IsEmail()
   email: string;
 
   @ApiProperty({ example: '123321' })
+  @IsNotEmpty()
   password: string;
 }
 
 export class SignInBodyDto {
   @ApiProperty({ example: 'test@gmail.com' })
+  @IsEmail()
   email: string;
 
   @ApiProperty({ example: '123321' })
+  @IsNotEmpty()
   password: string;
 }
 
@@ -21,6 +26,7 @@ export class GetSessionInfoDto {
   id: number;
 
   @ApiProperty({ example: 'test@gmail.com' })
+  @IsEmail()
   email: string;
 
   @ApiProperty()
