a1d1618c031c04b19dba59ee5bab25ac06417a4b
3str-webdev <3str.webdev@gmail.com>
2023-10-16 17:30:54 +0300

    feat: add sign-in, sign-up, sign-out and session endpoints

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,73 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse } from '@nestjs/swagger';
+import { Response } from 'express';
+import { AuthService } from './auth.service';
+import { GetSessionInfoDto, SignInBodyDto, SignUpBodyDto } from './dto';
+import { CookieService } from './cookie.service';
+import { AuthGuard } from './auth.guard';
+import { SessionInfo } from './session-info.decorator';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private cookieService: CookieService,
+  ) {}
+
+  @Post('sign-up')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiCreatedResponse()
+  async signUp(
+    @Body() body: SignUpBodyDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken } = await this.authService.signUp(
+      body.email,
+      body.password,
+    );
+
+    this.cookieService.setToken(res, accessToken);
+  }
+
+  @Post('sign-in')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse()
+  async signIn(
+    @Body() body: SignInBodyDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken } = await this.authService.signIn(
+      body.email,
+      body.password,
+    );
+
+    this.cookieService.setToken(res, accessToken);
+  }
+
+  @Post('sign-out')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse()
+  @UseGuards(AuthGuard)
+  signOut(@Res({ passthrough: true }) res: Response) {
+    this.cookieService.removeToken(res);
+  }
+
+  @Get('session')
+  @HttpCode(HttpStatus.OK)
+  @ApiOkResponse({
+    type: GetSessionInfoDto,
+  })
+  @UseGuards(AuthGuard)
+  getSessionInfo(@SessionInfo() session: GetSessionInfoDto) {
+    return session;
+  }
+}
