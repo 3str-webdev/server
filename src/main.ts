40adc4d45ec78028996f5da224e9046c33386237
3str-webdev <3str.webdev@gmail.com>
2023-10-16 10:37:48 +0300

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const document = new DocumentBuilder().setTitle('Web block service').build();
+  const config = new DocumentBuilder().setTitle('Web block service').build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
