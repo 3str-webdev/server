f0df5b332b4d694d6fe0ce7d02766bda80687d11
3str-webdev <3str.webdev@gmail.com>
2023-10-17 17:53:14 +0300

    feat: add creating block list with creating user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { AccountService } from 'src/account/account.service';
+import { BlockListService } from 'src/block-list/block-list.service';
 import { DbService } from 'src/db/db.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private db: DbService, private accountService: AccountService) {}
+  constructor(
+    private db: DbService,
+    private accountService: AccountService,
+    private blockListService: BlockListService,
+  ) {}
 
   async findByEmail(email: string) {
     return await this.db.user.findFirst({ where: { email } });
@@ -19,7 +24,10 @@ export class UsersService {
       },
     });
 
-    await this.accountService.create(user.id);
+    await Promise.all([
+      this.accountService.create(user.id),
+      this.blockListService.create(user.id),
+    ]);
 
     return user;
   }
